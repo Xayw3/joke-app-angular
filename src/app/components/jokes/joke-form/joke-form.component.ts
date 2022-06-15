fbff573322b61d3b4980a518d116a0ce76c6a2e1
Xayw3 <xaywe1@gmail.com>
2022-06-15 16:05:06 +0300

    created form

--- src/app/components/jokes/joke-form/joke-form.component.ts
+++ src/app/components/jokes/joke-form/joke-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-joke-form',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./joke-form.component.scss']
 })
 export class JokeFormComponent implements OnInit {
+  jokeForm: FormGroup = this.fb.group({});
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {};
 
   ngOnInit(): void {
-  }
+    this.buildForm()
+  };
 
+  buildForm(): void {
+    this.jokeForm = this.fb.group({
+      question: "",
+      answer: ""
+    })
+  };
+
+  addJoke(): void {
+    console.log(this.jokeForm.value.answer);
+  };
 }

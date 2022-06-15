80fef24b0697a329d7b401a3cf056d5b271c10c6
Xayw3 <xaywe1@gmail.com>
2022-06-15 15:02:35 +0300

    Create jokes components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JokesComponent } from './components/jokes/jokes.component';
+import { JokeListComponent } from './components/jokes/joke-list/joke-list.component';
+import { JokeFormComponent } from './components/jokes/joke-form/joke-form.component';
+import { JokeCardComponent } from './components/jokes/joke-card/joke-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokesComponent,
+    JokeListComponent,
+    JokeFormComponent,
+    JokeCardComponent,
   ],
   imports: [
     BrowserModule,
